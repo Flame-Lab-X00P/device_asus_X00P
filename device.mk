db46c050a57b069da964401c01bf28d1c7cb57bf
Flamefusion <meshekharbehera613@gmail.com>
2020-08-02 22:01:40 +0530

    X00P:Include prebuilt POSP dirac-equalizer Equilizer version: 1.0.0 Signed-off-by: Flamefusion <meshekharbehera613@gmail.com>

--- device.mk
+++ device.mk
@@ -160,6 +160,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey
+    
+# POSP Equalizer
+PRODUCT_PACKAGES += \
+    Equalizer
 
 # FM
 PRODUCT_PACKAGES += \
